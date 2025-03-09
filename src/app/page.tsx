a0d97e4ddcf6306e6e282695700b34dbc88fc302
starhn87 <starhn87@gmail.com>
2025-03-09 20:48:39 +0900

    feat: page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,27 @@
 import { DisplayModeButton } from "@/components/DisplayModeButton";
 import { getPages } from "@/utils/notion";
+import BlogListItem from "@/components/BlogListItem";
+import blogListItemInfoConverter from "@/utils/blogListItemInfoConverter";
+import { PageObjectResponse } from "@notionhq/client/build/src/api-endpoints";
 
 export default async function Home() {
-  const blogs = await getPages();
+  const { results: blogs } = await getPages();
 
   return (
-    <div>
-      <DisplayModeButton />
-    </div>
+    <>
+      <div className={"m-10 flex flex-col gap-5"}>
+        <div className={"flex justify-end right-0"}>
+          <DisplayModeButton />
+        </div>
+        <div className={"grid place-items-center gap-10"}>
+          <h1 className={"font-bold text-4xl"}>이승우의 블로그</h1>
+          <div className={"flex-row"}>
+            {blogs.map((blog: PageObjectResponse) => {
+              return <BlogListItem {...blogListItemInfoConverter(blog)} />;
+            })}
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
