0d5c82fd9c5671cf103763a767b6ff7a3b5b7a19
starhn87 <starhn87@gmail.com>
2025-04-20 23:08:50 +0900

    chore: remove pages package

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,3 @@
-import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -11,9 +10,5 @@ const nextConfig = {
     ],
   },
 };
-
-if (process.env.NODE_ENV === 'development') {
-  await setupDevPlatform();
-}
 
 export default nextConfig;
