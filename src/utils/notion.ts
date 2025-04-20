3dcbecc441d5cdca9ee60ea32ea1d44621098321
starhn87 <starhn87@gmail.com>
2025-04-20 21:45:25 +0900

    fix: env string guard

--- src/utils/notion.ts
+++ src/utils/notion.ts
@@ -3,26 +3,24 @@ import {
   BlockObjectResponse,
   PageObjectResponse,
 } from "@notionhq/client/build/src/api-endpoints";
-import { cache } from "react";
 
-export const notionClient = new Client({
-  auth: process.env.NEXT_PUBLIC_NOTION_TOKEN,
-});
+const NOTION_TOKEN = process.env.NEXT_PUBLIC_NOTION_TOKEN;
+if (!NOTION_TOKEN) throw new Error("NEXT_PUBLIC_NOTION_TOKEN is not set");
 
-export const getPages = cache(() => {
-  return notionClient.databases.query({
-    database_id: process.env.NEXT_PUBLIC_NOTION_DB_ID,
-  });
-});
+const NOTION_DB_ID = process.env.NEXT_PUBLIC_NOTION_DB_ID;
+if (!NOTION_DB_ID) throw new Error("NEXT_PUBLIC_NOTION_DB_ID is not set");
 
-export const getPageContent = cache((pageId: string) => {
-  return notionClient.blocks.children
-    .list({ block_id: pageId })
-    .then((res) => res.results as BlockObjectResponse[]);
-});
+export const notionClient = new Client({ auth: NOTION_TOKEN });
 
-export const getPage = cache((id: string) => {
-  return notionClient.pages.retrieve({
-    page_id: id,
-  });
-});
+export async function getPages() {
+  return notionClient.databases.query({ database_id: NOTION_DB_ID });
+}
+
+export async function getPage(id: string) {
+  return notionClient.pages.retrieve({ page_id: id });
+}
+
+export async function getPageContent(id: string) {
+  const blocks = await notionClient.blocks.children.list({ block_id: id });
+  return blocks.results as BlockObjectResponse[];
+}
